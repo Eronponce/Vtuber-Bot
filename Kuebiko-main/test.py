0c26fefd5cbe9c8ddb4ba300afd03c528e789dcb
Eron <81422635+Eronponce@users.noreply.github.com>
2024-01-19 23:50:53 -0300

    atualizacoes

--- Kuebiko-main/test.py
+++ Kuebiko-main/test.py
@@ -1,16 +1,2 @@
-# Example: reuse your existing OpenAI setup
-from openai import OpenAI
-
-# Point to the local server
-client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
-
-completion = client.chat.completions.create(
-  model="local-model", # this field is currently unused
-  messages=[
-    {"role": "system", "content": "Always answer in rhymes."},
-    {"role": "user", "content": "Introduce yourself."}
-  ],
-  temperature=0.7,
-)
-
-print(completion.choices[0].message)
+from rvc_infer import rvc_convert
+rvc_convert(model_path= "anya.pth",input_path= '/output.mp3')
